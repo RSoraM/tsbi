81e0d3a6f33e85b9e979a97f76b0944a686d28e8
RSM <rsm@RSM-MacBook-Pro.local>
2023-03-03 14:40:30 +0800

    test: 💍 修改测试脚本的Demo

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-export const log = (...args: any[]) => {
-  console.log(...args)
-}
-
-/**
- * 返回 x + y 的值
- * @param x Number(x)
- * @param y Number(y)
- * @returns Number(x) + Number(y)
- */
-export const add = (x: number, y: number) => Number(x) + Number(y)
+export * from './utilities/sha1'
---
+++ test/index.test.ts
@@ -0,0 +1,7 @@
+import { expect, describe, it } from 'vitest'
+
+describe('Test Demo', () => {
+  it('Hello, World', () => {
+    expect('Hello, World').toBe('Hello, World')
+  })
+})
